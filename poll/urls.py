6a781eb77b8b1214809c9a6da6a2a16b3f778a4d
email <tlsqja1120@gmail.com>
2021-10-12 09:22:55 +0900

    polls 파일

---
+++ poll/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from poll import views
+
+app_name = 'poll'
+
+urlpatterns = [
+    # poll/
+    path('', views.index, name='poll'),
+    # poll/<int:questing_id>/
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/result/', views.result, name='result'),
+]
